fcd97f17ba7726c185c4a9fa98e39a10f253bd70
blitzDing <johnbernard@hotmail.de>
2024-01-02 23:02:41 +0100

    I think i got the pom right now. Need to Dig.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module jborg.lightning
     requires transitive javafx.controls;
 	requires javafx.graphics;
 	requires javafx.base;
+	requires org.junit.jupiter.api;
 	
     exports jborg.lightning;
 }
